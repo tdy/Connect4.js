90c4fe023fbc1ee03b2aee87980a87bdc7c941f1
Rodion Efremov <coderodd3@yandex.com>
2024-06-14 18:23:35 +0300

    Filled the heuristic function.

--- public_html/js/connect4.js
+++ public_html/js/connect4.js
@@ -4,6 +4,7 @@ const MARGIN_RADIUS = 10;
 const PROGRESS_BAR_HEIGHT = 15;
 const O = 0;
 const X = 1;
+const EMPTY = -1;
 const BOARD_FILL_STYLE = '#3879e0';
 
 function getCanvas() {
@@ -28,6 +29,19 @@ class ConnectFourBoard {
     static #VICTORY_LENGTH = 4;
     
     #boardData;
+        
+    #getCellChar(cellCharacter) {
+        switch (cellCharacter) {
+            case X:
+                return "X";
+                
+            case O:
+                return "O";
+                
+            case EMPTY:
+                return " ";
+        }
+    }
     
     constructor(other) {
         if (arguments.length === 1) {
@@ -35,7 +49,19 @@ class ConnectFourBoard {
         } else {
             this.#boardData = Array(ConnectFourBoard.#ROWS *
                                     ConnectFourBoard.#COLUMNS);
+                            
+            for (let i = 0, end = this.#boardData.length; i < end; i++) {
+                this.#boardData[i] = EMPTY;
+            }
         }
+    }
+    
+    get(x, y) {
+        return this.#boardData[y * ConnectFourBoard.#COLUMNS + x];
+    }
+    
+    set(x, y, playerType) {
+        this.#boardData[y * ConnectFourBoard.#COLUMNS + x] = playerType;
     }
     
     toString() {
@@ -44,7 +70,7 @@ class ConnectFourBoard {
         for (let y = 0; y < ConnectFourBoard.#ROWS; y++) {
             for (let x = 0; x < ConnectFourBoard.#COLUMNS; x++) {
                 str += "|";
-                str += this.#getCellChar(this.#get(x, y));
+                str += this.#getCellChar(this.get(x, y));
             }
             
             str += "|\n";
@@ -56,23 +82,438 @@ class ConnectFourBoard {
         return str;
     }
     
-    #get(x, y) {
-        return this.#boardData[y * ConnectFourBoard.#COLUMNS + x];
+    isTie() {
+        for (let x = 0; x < COLUMNS; x++) {
+            if (!get(x, 0)) {
+                return false;
+            }
+        }
+        
+        return true;
     }
     
-    #getCellChar(cellCharacter) {
-        switch (cellCharacter) {
-            case X:
-                return "X";
-                
-            case O:
-                return "O";
-                
-            default:
-                return " ";
+    makePly(x, playerType) {
+        for (let y = ROWS - 1; y >= 0; y--) {
+            if (!get(x, y)) {
+                set(x, y, playerType);
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    unmakePly(x) {
+        for (let y = 0; y < ROWS; y++) {
+            if (get(x, y)) {
+                set(x, y, undefined);
+                return;
+            }
         }
     }
+    
+    hasHorizontalStreak(playerType, length) {
+        const lastX = COLUMNS - length;
+        
+        for (let y = ROWS - 1; y >= 0; y--) {
+            horizontalCheck:
+            
+            for (let x = 0; x <= lastX; x++) {
+                
+                for (let i = 0; i < length; i++) {
+                    if (this.get(x + i, y) !== playerType) {
+                        continue horizontalCheck;
+                    }
+                }
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    hasVerticalStreak(playerType, length) {
+        const lastY = ROWS - length;
+        
+        for (let x = 0; x < COLUMNS; x++) {
+            verticalCheck:
+            
+            for (let y = 0; y <= lastY; y++) {
+                for (let i = 0; i < length; i++) {
+                    if (this.get(x, y + i) !== playerType) {
+                        continue verticalCheck;
+                    }
+                }
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    hasAscendingDiagonalStreak(playerType, length) {
+        
+        const lastX = COLUMNS - length;
+        const lastY = length - 1;
+        
+        for (let y = ROWS - 1; y >= lastY; y--) {
+            diagonalCheck:
+            
+            for (let x = 0; x <= lastX; x++) {
+                for (let i = 0; i < length; i++) {
+                    if (this.get(x + i, y - i) !== playerType) {
+                        continue diagonalCheck;
+                    }
+                }
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    hasDescendingDiagonalStreak(playerType, length) {
+        
+        const firstX = length - 1;
+        const lastY  = length - 1;
+        
+        for (let y = ROWS - 1; y >= lastY; y--) {
+            diagonalCheck:
+                    
+            for (let x = firstX; x < COLUMNS; x++) {
+                for (let i = 0; i < length; i++) {
+                    if (this.get(x - i, y - i) !== playerType) {
+                        continue diagonalCheck;
+                    }
+                }
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    isWinningFor(playerType) {
+        return 
+           this.hasAscendingDiagonalStreak  (playerType, VICTORY_LENGTH) ||
+           this.hasDescendingDiagonalStreak (playerType, VICTORY_LENGTH) ||
+           this.hasHorizontalStreak         (playerType, VICTORY_LENGTH) ||
+           this.hasVerticalStreak           (playerType, VICTORY_LENGTH);
+    }
+    
+    tryLoadAscendingWinningPattern(playerType, length) {
+        
+        const lastX = COLUMNS - length;
+        const lastY = length - 1;
+        const winningPattern = [];
+        
+        for (let y = ROWS - 1; y >= lastY; y--) {
+            diagonalCheck:
+                    
+            for (let x = 0; x <= lastX; x++) {
+                for (let i = 0; i < length; i++) {
+                    if (this.get(x + i, y - i) === playerType) {
+                        winningPattern.push({x: x + i, y: y - i});
+                        
+                        if (winningPattern.length === length) {
+                            return winningPattern;
+                        }
+                    } else {
+                        winningPattern = [];
+                        continue diagonalCheck;
+                    }
+                }
+            }
+        }
+        
+        return null;
+    }
+    
+    tryLoadDescendingWinningPattern(playerType, length) {
+        
+        const firstX = length - 1;
+        const lastY = ROWS - length;
+        const winningPattern = [];
+        
+        for (let y = ROWS - 1; y > lastY; y--) {
+            diagonalCheck:
+                    
+            for (let x = firstX; x < COLUMNS; x++) {
+                for (let i = 0; i < length; i++) {
+                    if (this.get(x - i, y - i) === playerType) {
+                        winningPattern.push({x: x - i, y: y - i});
+                        
+                        if (winningPattern.length === length) {
+                            return winningPattern;
+                        }
+                    } else {
+                        winningPattern = [];
+                        continue diagonalCheck;
+                    }
+                }
+            }
+        }
+        
+        return null;
+    }
+    
+    tryLoadHorizontalWinningPattern(playerType, length) {
+        
+        const lastX = COLUMNS - length;
+        const winningPattern = [];
+        
+        for (let y = ROWS - 1; y >= 0; y--) {
+            horizontalCheck:
+                    
+            for (let x = 0; x <= lastX; x++) {
+                for (let i = 0; i < length; i++) {
+                    if (this.get(x + i, y) === playerType) {
+                        winningPattern.push({x: x + i, y: y});
+                        
+                        if (winningPattern.length === length) {
+                            return winningPattern;
+                        }
+                    } else {
+                        winningPattern = [];
+                        continue horizontalCheck;
+                    }
+                }
+            }
+        }
+        
+        return null;
+    }
+    
+    tryLoadVerticalWinningPattern(playerType, length) {
+        
+        const lastY = ROWS - length;
+        const winningPattern = [];
+        
+        for (let x = 0; x < COLUMNS; x++) {
+            verticalCheck:
+                    
+            for (let y = 0; y <= lastY; y++) {
+                for (let i = 0; i < length; i++) {
+                    if (this.get(x, y + i) === playerType) {
+                        winningPattern.push({x: x, y: y + i});
+                
+                        if (winningPattern.length === length) {
+                            return winningPattern;
+                        }
+                    } else {
+                        winningPattern = [];
+                        continue verticalCheck;
+                    }
+                }
+            }
+        }
+        
+        return null;
+    }
 };
+
+class ConnectFourHeuristicFunction {
+    static #TWO_BLOCKS_SCORE = 1.0;
+    static #THREE_BLOCKS_SCORE = 10.0;
+    static #MINIMIZING_PLAYER_VICTORY_SCORE = -10E6;
+    static #MAXIMIZING_PLAYER_VICTORY_SCORE = +10E6;
+    
+    evaluate(state, depth) {
+        if (state.isWinningFor(X)) {
+            return ConnectFourHeuristicFunction.#MINIMIZING_PLAYER_VICTORY_SCORE - depth;
+        }
+        
+        if (state.isWinningFor(O)) {
+            return ConnectFourHeuristicFunction.#MAXIMIZING_PLAYER_VICTORY_SCORE + depth;
+        }
+        
+        return evaluate2(state) + evaluate3(state);
+    }
+    
+    #evaluate2(state) {
+        return #evaluate2Horizontal(state) +
+               #evaluate2Vertical(state) + 
+               #evaluate2Ascending(state) +
+               #evaluate2Descending(state);
+    }
+    
+    #evaluate3(state) {
+        return #evaluate3Horizontal(state) +
+               #evaluate3Vertical(state) + 
+               #evaluate3Ascending(state) +
+               #evaluate3Descending(state);
+    }
+    
+    #evaluate2Horizontal(state) {
+        let sum = 0.0;
+        
+        for (let y = 0; y < ROWS; y++) {
+            for (let x = 0; x < COLUMNS - 1; x++) {
+                if (state.get(x, y) === O &&
+                    state.get(x + 1, y) === O) {
+                    sum += #TWO_BLOCKS_SCORE;
+                } else if (state.get(x, y) === X &&
+                       state.get(x + 1, y) === X) {
+                    
+                    sum -= #TWO_BLOCKS_SCORE;
+                }
+            }
+        }
+        
+        return sum;
+    }
+    
+    #evaluate2Vertical(state) {
+        let sum = 0.0;
+        
+        for (let y = 0; y < ROWS - 1; y++) {
+            for (let x = 0; x < COLUMNS; x++) {
+                if (state.get(x, y) === O &&
+                    state.get(x, y + 1) === O) {
+                    
+                    sum += #TWO_BLOCKS_SCORE;
+                } else if (state.get(x, y) === X &&
+                       state.get(x, y + 1) === X) {
+                    
+                    sum -= #TWO_BLOCKS_SCORE;
+                }
+            }
+        }
+        
+        return sum;
+    }
+    
+    #evaluate2Ascending(state) {
+        let sum = 0.0;
+        
+        for (let y = ROWS - 1; y > 0; y--) {
+            for (let x = 0; x < COLUMNS - 1; x++) {
+                if (state.get(x, y) === O &&
+                    state.get(x + 1, y - 1) === O) {
+                    sum += #TWO_BLOCKS_SCORE;
+                } else if (state.get(x, y) === X &&
+                           state.get(x + 1, y - 1) === X) {
+                    
+                    sum -= #TWO_BLOCKS_SCORE;
+                }
+            }
+        }
+        
+        return sum;
+    }
+    
+    #evaluate2Descending(state) {
+        let sum = 0.0;
+        
+        for (let y = ROWS - 1; y > 0; y--) {
+            for (let x = 1; x < COLUMNS; x++) {
+                if (state.get(x, y) === O &&
+                    state.get(x - 1, y - 1) === O) {
+                    
+                    sum += #TWO_BLOCKS_SCORE;
+                } else if (state.get(x, y) === X &&
+                           state.get(x - 1, y - 1) === X) {
+                    
+                    sum -= #TWO_BLOCKS_SCORE;
+                }
+            }
+        }
+        
+        return sum;
+    }
+    
+    #evaluate3Horizontal(state) {
+        let sum = 0.0;
+        
+        for (let y = 0; y < ROWS; y++) {
+            for (let x = 0; x < COLUMNS - 2; x++) {
+                if (state.get(x, y) === O &&
+                    state.get(x + 1, y) === O &&
+                    state.get(x + 2, y) === O) {
+                    
+                    sum += #THREE_BLOCKS_SCORE;
+                } else if (state.get(x, y) === X &&
+                        state.get(x + 1, y) === X &&
+                        state.get(x + 2, y) === X) {
+                    
+                    sum -= #THREE_BLOCKS_SCORE;
+                }
+            }
+        }
+        
+        return sum;
+    }
+    
+    #evaluate3Vertical(state) {
+        let sum = 0.0;
+        
+        for (let y = 0; y < ROWS - 2; y++) {
+            for (let x = 0; x < COLUMNS; x++) {
+                if (state.get(x, y) === O &&
+                    state.get(x, y + 1) === O &&
+                    state.get(x, y + 2) === O) {
+                    
+                    sum += #THREE_BLOCKS_SCORE;
+                } else if (state.get(x, y) === X &&
+                        state.get(x, y + 1) === X &&
+                        state.get(x, y + 2) === X) {
+                    
+                    sum -= #THREE_BLOCKS_SCORE;
+                }
+            }
+        }
+        
+        return sum;
+    }
+    
+    #evaluate3Ascending(state) {
+        let sum = 0.0;
+        
+        for (let y = ROWS - 1; y > 1; y--) {
+            for (let x = 0; x < COLUMNS - 2; x++) {
+                if (state.get(x, y) === O &&
+                    state.get(x + 1, y - 1) === O &&
+                    state.get(x + 2, y - 2) === O) {
+                    
+                    sum += #THREE_BLOCKS_SCORE;
+                } else if (state.get(x, y) === X &&
+                        state.get(x + 1, y - 1) === X && 
+                        state.get(x + 2, y - 2) === X) {
+                    
+                    sum -= #THREE_BLOCKS_SCORE;
+                }
+            }
+        }
+        
+        return sum;
+    }
+    
+    #evaluate3Descending(state) {
+        let sum = 0.0;
+        
+        for (let y = ROWS - 1; y > 1; y--) {
+            for (let x = 2; x < COLUMNS; x++) {
+                if (state.get(x, y) === O &&
+                    state.get(x - 1, y - 1) === O &&
+                    state.get(x - 2, y - 2) === O) {
+                    
+                    sum += #THREE_BLOCKS_SCORE;
+                } else if (state.get(x, y) === X &&
+                        state.get(x - 1, y - 1) === X &&
+                        state.get(x - 2, y - 2) === X) {
+                    
+                    sum -= #THREE_BLOCKS_SCORE;
+                }
+            }
+        }
+        
+        return sum;
+    }
+}
 
 (function() {
     
